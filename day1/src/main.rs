8a20a39555217e79c5e4535cccb2f950fa1cd91f
Andy LeClair <andyleclair@gmail.com>
2018-12-01 14:36:26 -0500

    refactor

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -3,6 +3,6 @@ mod problem2;
 
 fn main() {
     println!("Advent of code 2018 Day 1 problem 1");
-    problem1::problem1::solve();
-    problem2::problem2::solve();
+    problem1::solve();
+    problem2::solve();
 }
--- day1/src/problem2.rs
+++ day1/src/problem2.rs
@@ -1,68 +1,71 @@
-pub mod problem2 {
+use std::collections::HashMap;
+use std::collections::VecDeque;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 
-    use std::fs::File;
-    use std::io::{BufRead, BufReader};
-    use std::collections::HashMap;
-    use std::collections::VecDeque;
+const STARTING_FREQ: i32 = 0;
 
-    const STARTING_FREQ: i32 = 0;
+pub fn solve() {
+    let mut seen_frequencies: HashMap<i32, i32> = HashMap::new();
+    let mut current_freq = STARTING_FREQ;
+    seen_frequencies.insert(current_freq, 1);
+    let freq_ring = populate_ring();
+    let found = false;
 
-    pub fn solve() {
-        let mut seen_frequencies: HashMap<i32, i32> = HashMap::new();
-        let mut current_freq = STARTING_FREQ;
-        seen_frequencies.insert(current_freq, 1);
-        let freq_ring = populate_ring();
-        let mut found = false;
-
-        while !found {
-            for freq in freq_ring.iter() {
-                match freq.sign {
-                    Sign::Plus => current_freq = current_freq + freq.value,
-                    Sign::Minus => current_freq = current_freq - freq.value,
-                }
-                if seen_frequencies.contains_key(&current_freq) {
-                    println!("Found second value {}", current_freq);
-                    found = true;
-                } else {
-                    seen_frequencies.insert(current_freq, 1);
-                }
+    while !found {
+        for freq in freq_ring.iter() {
+            match freq.sign {
+                Sign::Plus => current_freq = current_freq + freq.value,
+                Sign::Minus => current_freq = current_freq - freq.value,
+            }
+            if seen_frequencies.contains_key(&current_freq) {
+                println!("Found second value {}", current_freq);
+                return;
+            } else {
+                seen_frequencies.insert(current_freq, 1);
             }
         }
     }
+}
 
-    fn parse_line(line: &str) -> Freq {
-        let sign = String::from(&line[..1]);
-        let value: i32 = String::from(&line[1..]).parse().unwrap();
-        match sign.as_ref() {
-            "+" => Freq { sign: Sign::Plus, value: value },
-            "-" => Freq { sign: Sign::Minus, value: value },
-            _ => panic!("Could not parse sign"),
-        }
+fn parse_line(line: &str) -> Freq {
+    let sign = String::from(&line[..1]);
+    let value: i32 = String::from(&line[1..]).parse().unwrap();
+    match sign.as_ref() {
+        "+" => Freq {
+            sign: Sign::Plus,
+            value: value,
+        },
+        "-" => Freq {
+            sign: Sign::Minus,
+            value: value,
+        },
+        _ => panic!("Could not parse sign"),
     }
+}
 
-    fn populate_ring() -> VecDeque<Freq> {
-        let freq_file = "frequencies.txt";
-        let f = File::open(freq_file).expect("Unable to open file");
-        let f = BufReader::new(f);
-        let mut ring: VecDeque<Freq> = VecDeque::new();
+fn populate_ring() -> VecDeque<Freq> {
+    let freq_file = "frequencies.txt";
+    let f = File::open(freq_file).expect("Unable to open file");
+    let f = BufReader::new(f);
+    let mut ring: VecDeque<Freq> = VecDeque::new();
 
-        for line in f.lines() {
-            let line  = String::from(line.unwrap());
-            let parsed_freq = parse_line(&line);
-            ring.push_back(parsed_freq);
-        }
-        return ring;
+    for line in f.lines() {
+        let line = String::from(line.unwrap());
+        let parsed_freq = parse_line(&line);
+        ring.push_back(parsed_freq);
     }
+    return ring;
+}
 
-    #[derive(Debug)]
-    struct Freq {
-        sign: Sign,
-        value: i32,
-    }
+#[derive(Debug)]
+struct Freq {
+    sign: Sign,
+    value: i32,
+}
 
-    #[derive(Debug)]
-    enum Sign {
-        Plus,
-        Minus,
-    }
+#[derive(Debug)]
+enum Sign {
+    Plus,
+    Minus,
 }
